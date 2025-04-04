a32efb4af6668b7f131612013badd091d2fde610
talalbutt56 <talal_b56@hotmail.com>
2025-04-04 12:51:01 -0400

    Update server.js

--- server.js
+++ server.js
@@ -74,3 +74,9 @@ app.put('/api/vehicles/:id', async (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
